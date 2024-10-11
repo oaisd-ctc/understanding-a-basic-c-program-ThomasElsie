a599070ee3ceb507ca29ec4717738896257df5ff
ThomasElsie <theld56@student.oaisd.org>
2024-10-11 13:27:14 +0000

    I finally did it! I kinda understand C SHARP :0

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("My name is Thomas Elsie.");
+
+        //using variables
+        int age = 18;
+        string greeting = "Hello World";
+        string firstname = "Thomas";
+        string lastname = "Elsie";
+        double money = 2.79;
+
+        Console.WriteLine(greeting + "!" + "My name is " + firstname + " "
+        + lastname + ". I am " + age + " years old. I just spent $" + money + " on an Energy Drink.");
+    }
+}
